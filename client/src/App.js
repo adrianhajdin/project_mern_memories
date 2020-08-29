cd5db2e1df229d1d08c0872603116e5ee0f85776
adrianhajdin <adrianhajdin00@gmail.com>
2020-08-29 10:48:12 +0200

    final push

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
+import memories from './images/memories.png';
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
@@ -20,6 +21,7 @@ const App = () => {
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
--- client/src/styles.js
+++ client/src/styles.js
@@ -4,8 +4,15 @@ export default makeStyles(() => ({
   appBar: {
     borderRadius: 15,
     margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   heading: {
     color: 'rgba(0,183,255, 1)',
+  },
+  image: {
+    marginLeft: '15px',
   },
 }));
