227c031c6fdbbb0a6841f6ef24c6292f3d118384
Kartik Singh <kssklol925@gmail.com>
2020-12-12 23:27:44 +0530

    fixes time

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,21 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
-    title: String,
-    message: String,
-    creator: String,
-    tags: [String],
-    selectedFile: String,
-    likeCount: {
-        type: Number,
-        default: 0,
-    },
-    createdAt: {
-        type: Date,
-        default: new Date(),
-    },
-})
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-var PostMessage = mongoose.model('PostMessage', postSchema);
+var PostMessage = mongoose.model("PostMessage", postSchema);
 
 export default PostMessage;
