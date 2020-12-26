3428e87225257b90194bbb0fba53b40ec1dc2a93
Adrian Hajdin <adrianhajdin00@gmail.com>
2020-12-26 19:29:13 +0100

    PART 3

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());S
+app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
