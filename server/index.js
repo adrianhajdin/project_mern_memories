c15e4a81fddad616d74fa9213acd8a3dd602ab72
Adrian Hajdin <adrianhajdin00@gmail.com>
2020-12-26 19:28:44 +0100

    PART 3

--- server/index.js
+++ server/index.js
@@ -11,12 +11,12 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors());S
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://root:4AacDf9Tva@memories-mongo-production/admin?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb://root:4AacDf9Tva@memories-mongo-production/admin?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
