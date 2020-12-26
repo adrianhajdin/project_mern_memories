f475941c4a05273f3365df22b34c3ac62ac9834a
Adrian Hajdin <adrianhajdin00@gmail.com>
2020-12-26 17:33:58 +0100

    PART 3

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memories-backend.zeet.app' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://root:4AacDf9Tva@memories-mongo-production?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
