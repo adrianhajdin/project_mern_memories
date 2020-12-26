7915b0e8a44ec18a0ef9858284df2c787b583ad4
Adrian Hajdin <adrianhajdin00@gmail.com>
2020-12-26 19:06:21 +0100

    PART 3

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from "dotenv";
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
@@ -17,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://root:4AacDf9Tva@memories-mongo-production?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://root:4AacDf9Tva@memories-mongo-production/admin?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
