a9b7bbd18ef7e32c0522579a5195b80a55e2c242
davebanguilan <desbanguilan@gmail.com>
2021-01-23 19:28:37 +0800

    Fix for mobile responsiveness

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { Container, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-import { getPosts } from '../../actions/posts';
-import Posts from '../Posts/Posts';
-import Form from '../Form/Form';
+import { getPosts } from "../../actions/posts";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+import useStyles from "./styles";
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts());
@@ -17,7 +19,13 @@ const Home = () => {
   return (
     <Grow in>
       <Container>
-        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+        <Grid
+          className={classes.mainContainer}
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
           <Grid item xs={12} sm={7}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
