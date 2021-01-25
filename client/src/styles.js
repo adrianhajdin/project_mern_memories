1b2d553ecde55e343e1026dfc2bca496202e3646
Adrian Hajdin <adrianhajdin00@gmail.com>
2021-01-25 10:04:35 +0100

    test

---
+++ client/src/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  [theme.breakpoints.down('sm')]: {
+    mainContainer: {
+      flexDirection: 'column-reverse',
+    },
+  },
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgba(0,183,255, 1)',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
