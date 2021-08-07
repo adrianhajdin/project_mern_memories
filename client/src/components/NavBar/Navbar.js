4ca4ad4ebbe24b8c67242c3171b0031a3721d3d5
Jatin Sonkusale <jatin22000@gmail.com>
2021-08-07 16:55:52 +0530

    remove warning

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -30,6 +30,7 @@ function Navbar() {
     }
 
     setUser(JSON.parse(localStorage.getItem("profile")));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
   return (
