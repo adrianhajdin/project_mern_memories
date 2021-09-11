4b051fea33cae9feeba15e0b83a269ab29efc03b
Adrian Hajdin - JavaScript Mastery <adrianhajdin00@gmail.com>
2021-09-11 12:55:13 +0200

    Update Post.js

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -19,7 +19,7 @@ const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
 
   const userId = user?.result.googleId || user?.result?._id;
-  const hasLikedPost = post.likes.find((like) => like === userId);
+  const hasLikedPost = post?.likes?.find((like) => like === userId);
 
   const handleLike = async () => {
     dispatch(likePost(post._id));
