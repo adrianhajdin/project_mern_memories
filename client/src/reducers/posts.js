d52adeeed923391c96acc56f673841ff17ce2023
Matin <85360666+MatinT-SA@users.noreply.github.com>
2022-03-15 16:13:06 +0330

    Fixed warning in posts.js
    This will fix the warning of "Assign arrow function to a variable before exporting as module default" in the posts.js file inside the reducers folder.

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,6 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
-export default (posts = [], action) => {
+consts reducers = (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload;
@@ -17,3 +17,4 @@ export default (posts = [], action) => {
   }
 };
 
+export default reducers;
