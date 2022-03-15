4992965e6a0aa999e8888b0cbcef1ddae7779659
Matin <85360666+MatinT-SA@users.noreply.github.com>
2022-03-15 16:16:09 +0330

    Update posts.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,6 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
-consts reducers = (posts = [], action) => {
+const reducers = (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload;
