d11229d1f2cd99936790372ce5579cf28c21de7c
Robinkphilip <robinphilipp8018@gmail.com>
2022-11-11 14:57:17 +0530

    redux_folder_struc_setup change

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
-import { reducers } from './reducers';
-import App from './App';
-import './index.css';
-
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
