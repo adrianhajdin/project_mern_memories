4108c618c0ce89e697b6657810c3df08a8f65736
Ved Teredesai <vedteredesai@gmail.com>
2025-06-23 14:01:20 +0530

    add new comment feature

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://vedteredesai:Qa6w5QX8kq0blv4q@cluster1.npsxphx.mongodb.net/memories?retryWrites=true&w=majority&appName=Cluster1';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost, commentPost } from '../controllers/posts.js';
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.get('/:id', getPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
 router.patch('/:id/likePost', likePost);
+router.post('/:id/commentPost', commentPost);
 
 export default router;
