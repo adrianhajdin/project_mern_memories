77c44980bc6aeb5fc45554d5e477a817678e5322
aryanmathur1911 <mathuraryan1119@gmail.com>
2025-08-20 18:31:12 +0530

    Fixed the issue of clear button which was not working.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -9,6 +9,10 @@ import { createPost, updatePost } from '../../actions/posts';
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
   const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+
+  // Ensure the file input is reset when the form is used, because the file input does not reset automatically
+  const [fileKey, setFileKey] = useState(Date.now())
+
   const dispatch = useDispatch();
   const classes = useStyles();
 
@@ -19,6 +23,7 @@ const Form = ({ currentId, setCurrentId }) => {
   const clear = () => {
     setCurrentId(0);
     setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    setFileKey(Date.now()); // Reset the file input by changing the key
   };
 
   const handleSubmit = async (e) => {
@@ -41,7 +46,7 @@ const Form = ({ currentId, setCurrentId }) => {
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
-        <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
+        <div className={classes.fileInput}><FileBase key={fileKey} type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
         <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
       </form>
